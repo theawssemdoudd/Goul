77d41c52f7955d0d54ed1c968ee5df8bb34e1763
Antoine Chassagne <antoine.chassagne@me.com>
2024-10-24 21:39:35 +0200

    feat: add error util

--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,5 @@
 export const wait = (delay = 5000) => new Promise(resolve => setTimeout(resolve, delay));
+
+export const error = (message = 'An error occurred') => {
+  throw new Error(message);
+}
