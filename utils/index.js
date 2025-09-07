0e27c6d502c0cbc6a239535c1dc9f1c96765726e
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-09-07 17:23:16 +0100

    Update and rename index.js to firebase.js

--- utils/index.js
+++
@@ -1,5 +0,0 @@
-export const wait = (delay = 5000) => new Promise(resolve => setTimeout(resolve, delay));
-
-export const error = (message = 'An error occurred') => {
-  throw new Error(message);
-}
