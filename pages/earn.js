863991d3d3f01ad2d3709d3a93f581adc984837c
theawssemdoudd <143346166+theawssemdoudd@users.noreply.github.com>
2025-09-07 23:52:00 +0100

    Update earn.js

--- pages/earn.js
+++ pages/earn.js
@@ -1,1 +1,8 @@
-
+export default function Earn() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold">💰 ربح المال</h1>
+      <p>ستظهر هنا المهام المتاحة من باقي المستخدمين.</p>
+    </div>
+  );
+}
